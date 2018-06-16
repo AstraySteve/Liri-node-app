97f7b847e7b7507b7832d60aad80f83b82a7f397
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-06-16 01:42:43 -0400

    spotify working, missing default action

--- liri.js
+++ liri.js
@@ -1,22 +1,21 @@
-require("dotenv").config(); //npm dotenv
-//TODO all require and imports here
-var keys = require("./keys.js");
-var fs = require("fs");
-
-//TODO ALL NPM IMPORTS HERE
-var Twitter = require('twitter'); //npm twitter
-var client = new Twitter(keys.twitter);
-
-var request = require('request'); //npm request
-
-//console.log(keys);
-
-/*TODO
-var Spotify = require('node-spotify-api'); //npm spotify
-var spotify = new Spotify(keys.spotify);
-
-
+/*
+    Steven Tran
+    Assignment 10, 2018
+    UofT SCS Coding Bootcamp
 */
+
+//require npm imports
+require("dotenv").config(); //npm dotenv
+var request = require('request'); //npm request
+var Twitter = require('twitter'); //npm twitter
+var Spotify = require('node-spotify-api'); //npm spotify
+var fs = require("fs");
+//Custom imports
+var keys = require("./keys.js");
+
+//Global Variables
+var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
 
 //Functions
 myTweets = () => {
@@ -37,22 +36,52 @@ myTweets = () => {
     });
 }
 
-movieInfo = () => {
-    //Function will call OMDB and display movie info to terminal/bash
+getSong = () => {
+    //Function determines what song to look up
+    if(process.argv.length < 4){
+        var songName = "The Sign";
+        var artistName = "Ace of Base"
+        songInfo(songName, artistName);
+    }
+    else{
+        var songName = process.argv[3];
+        songInfo(songName);
+    }
+}
+songInfo = (songName, artistName) => {
+    //Function request to Spotify API and displays information: Artist, Song name, Preview link, Album.
+    spotify.search({type: 'track', query: songName, limit: 1}, function(err,data){
+        if(err){
+            console.log("Error occurred: " + err);
+        }
+        else{
+            if(data.tracks.items.length > 0){
+                var info = data.tracks.items[0];
+                console.log(`Artist(s): ${info.artists[0].name}\nTrack: ${info.name}`);
+                console.log(`Preview: ${info.preview_url}\nAlbum: ${info.album.name}`);
+            }
+            else{
+                console.log("Song not found!");
+            }
+        }
+    });
+}
+
+getMovie = () => {
+    //Function determines what movie to look up
     if (process.argv.length < 4){
         var movieName = "Mr. Nobody";
         console.log("If you haven't watched 'Mr. Nobody.' then you should: http://www.imdb.com/title/tt0485947/");
         console.log("It's on Netflix!");
-        getMovie(movieName);
+        movieInfo(movieName);
     }
     else{
         var movieName = process.argv[3];
-        getMovie(movieName);
+        movieInfo(movieName);
     }
 }
-
-getMovie = (movieName) => {
-    /*Function will request OMDB and output the following info: Title, Year of release, IMDB rating,
+movieInfo = (movieName) => {
+    /*Function will request to OMDB and output the following info: Title, Year of release, IMDB rating,
       Rotten Tomatos Rating, Country of production, Lanugage, Plot, Actors*/
     var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
     //console.log(queryURL); //DEBUG CODE
@@ -67,6 +96,7 @@ getMovie = (movieName) => {
                 console.log(`Plot: ${info.Plot}\nActors: ${info.Actors}`);
             }
             else{
+                //Movie not found
                 console.log(info.Error);
             }
         }
@@ -79,10 +109,10 @@ switch(input){
         myTweets();
         break;
     case "spotify-this-song":
-        //something
+        getSong();
         break;
     case "movie-this":
-        movieInfo();
+        getMovie();
         break;
     case "do-what-it-says":
         //something
